13dc0945dad588c2d3d00fad4386aacbaa40c30f
twisted1919 <totyoserbangeorgecristian@yahoo.com>
2016-08-22 22:15:27 +0300

    add icon helper

---
+++ Icon.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace twisted1919\helpers;
+
+use yii\helpers\Html;
+
+class Icon
+{
+    /**
+     * @param $name
+     * @return string
+     */
+    public static function make($name)
+    {
+        $className = null;
+
+        if (strpos($name, 'glyphicon') === 0) {
+            $className = 'glyphicon ' . $name;
+        }
+
+        if (!$className && strpos($name, 'fa') === 0) {
+            $className = 'fa ' . $name;
+        }
+        
+        if (!$className) {
+            $icons     = self::getRegisteredActionIcons();
+            $className = isset($icons[$name]) ? $icons[$name] : null;
+        }
+
+        if (!$className) {
+            $className = 'fa fa-circle-o';
+        }
+
+        return '&nbsp;' . Html::tag('i', '&nbsp;', ['class' => $className]) . '&nbsp;';
+    }
+
+    /**
+     * @return array
+     */
+    protected static function getRegisteredActionIcons()
+    {
+        return [
+            'create' => 'fa fa-plus-square',
+            'update' => 'fa fa-pencil-square-o',
+            'view'   => 'fa fa-eye-open',
+            'delete' => 'fa fa-trash',
+            'refresh'=> 'fa fa-refresh',
+            'back'   => 'fa fa-arrow-circle-left',
+            'save'   => 'fa fa-save',
+        ];
+    }
+}
